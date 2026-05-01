d464817f61d49514a38583ae24db1c02af3188ad
thierroller55-source <thierroller55@gmail.com>
2026-05-01 01:55:27 +0000

    Passage aux emails Gmail

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,13 +1,28 @@
 
 
+// const nodemailer = require('nodemailer');
+
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     // On utilise les variables d'environnement pour cacher tes accès
+//     user: "thierroller55@gmail.com", 
+//     pass: "qiqsjkbeqezkxddk" 
+//   }
+// });
+
+// module.exports = transporter;
+
+
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // Utilisation de SSL (plus sécurisé pour Render)
   auth: {
-    // On utilise les variables d'environnement pour cacher tes accès
     user: "thierroller55@gmail.com", 
-    pass: "qiqsjkbeqezkxddk" 
+    pass: "qiqsjkbeqezkxddk"  
   }
 });
 
