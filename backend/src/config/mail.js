b7b8e2aef03a68a4a66e62472c0466ba79f0daf3
thierroller55-source <thierroller55@gmail.com>
2026-05-01 01:32:21 +0000

    Passage aux emails Gmail réels

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,12 +1,13 @@
+
+
 const nodemailer = require('nodemailer');
 
-// Configuration du transporteur (Moteur d'envoi)
 const transporter = nodemailer.createTransport({
-  host: "sandbox.smtp.mailtrap.io", // Remplace par tes infos Mailtrap
-  port: 2525,
+  service: 'gmail',
   auth: {
-    user: "762781dc9a234f", // À mettre dans ton .env
-    pass: "e585d9775b9af3"  // À mettre dans ton .env
+    // On utilise les variables d'environnement pour cacher tes accès
+    user: "thierroller55@gmail.com", 
+    pass: "qiqsjkbeqezkxddk" 
   }
 });
 
